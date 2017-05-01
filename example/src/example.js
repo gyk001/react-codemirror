6f7611a985dacf3affc2a35d3dbd118a2c27bb42
郭玉昆 <yukun.gyk@alibaba-inc.com>
2017-05-01 17:59:55 +0800

    upgrade React.createClass to es6  class

--- example/src/example.js
+++ example/src/example.js
@@ -11,35 +11,50 @@ var defaults = {
 	javascript: 'var component = {\n\tname: "react-codemirror",\n\tauthor: "Jed Watson",\n\trepo: "https://github.com/JedWatson/react-codemirror"\n};'
 };
 
-var App = React.createClass({
-	getInitialState () {
-		return {
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
 			code: defaults.markdown,
 			readOnly: false,
 			mode: 'markdown',
 		};
-	},
-	updateCode (newCode) {
+		this.updateCode = this.updateCode.bind(this);
+		this.changeMode = this.changeMode.bind(this);
+		this.toggleReadOnly = this.toggleReadOnly.bind(this);
+		this.interact = this.interact.bind(this);
+		this.updateCode = this.updateCode.bind(this);
+		this.updateCode = this.updateCode.bind(this);
+		this.updateCode = this.updateCode.bind(this);
+		this.updateCode = this.updateCode.bind(this);
+
+	}
+
+	updateCode(newCode) {
 		this.setState({
 			code: newCode
 		});
-	},
-	changeMode (e) {
+	}
+
+	changeMode(e) {
 		var mode = e.target.value;
 		this.setState({
 			mode: mode,
 			code: defaults[mode]
 		});
-	},
-	toggleReadOnly () {
+	}
+
+	toggleReadOnly() {
 		this.setState({
 			readOnly: !this.state.readOnly
 		}, () => this.refs.editor.focus());
-	},
-	interact (cm) {
+	}
+
+	interact(cm) {
 		console.log(cm.getValue());
-	},
-	render () {
+	}
+
+	render() {
 		var options = {
 			lineNumbers: true,
 			readOnly: this.state.readOnly,
@@ -47,17 +62,21 @@ var App = React.createClass({
 		};
 		return (
 			<div>
-				<Codemirror ref="editor" value={this.state.code} onChange={this.updateCode} options={options} interact={this.interact} />
-				<div style={{ marginTop: 10 }}>
+				<Codemirror ref="editor" value={this.state.code} onChange={this.updateCode} options={options}
+							interact={this.interact}/>
+				<div style={{marginTop: 10}}>
 					<select onChange={this.changeMode} value={this.state.mode}>
 						<option value="markdown">Markdown</option>
 						<option value="javascript">JavaScript</option>
 					</select>
-					<button onClick={this.toggleReadOnly}>Toggle read-only mode (currently {this.state.readOnly ? 'on' : 'off'})</button>
+					<button onClick={this.toggleReadOnly}>Toggle read-only mode
+						(currently {this.state.readOnly ? 'on' : 'off'})
+					</button>
 				</div>
 			</div>
 		);
 	}
-});
+
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- src/Codemirror.js
+++ src/Codemirror.js
@@ -1,60 +1,55 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const findDOMNode = ReactDOM.findDOMNode;
-const className = require('classnames');
-const debounce = require('lodash.debounce');
+'use strict';
 
-function normalizeLineEndings (str) {
+var React = require('react');
+var ReactDOM = require('react-dom');
+const PropTypes = require('prop-types');
+var findDOMNode = ReactDOM.findDOMNode;
+var className = require('classnames');
+var debounce = require('lodash.debounce');
+
+function normalizeLineEndings(str) {
 	if (!str) return str;
 	return str.replace(/\r\n|\r/g, '\n');
 }
 
-const CodeMirror = React.createClass({
-	propTypes: {
-		className: React.PropTypes.any,
-		codeMirrorInstance: React.PropTypes.func,
-		defaultValue: React.PropTypes.string,
-		onChange: React.PropTypes.func,
-		onFocusChange: React.PropTypes.func,
-		onScroll: React.PropTypes.func,
-		options: React.PropTypes.object,
-		path: React.PropTypes.string,
-		value: React.PropTypes.string,
-		preserveScrollPosition: React.PropTypes.bool,
-	},
-	getDefaultProps () {
-		return {
-			preserveScrollPosition: false,
+class Codemirror extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isFocused: false
 		};
-	},
-	getCodeMirrorInstance () {
-		return this.props.codeMirrorInstance || require('codemirror');
-	},
-	getInitialState () {
-		return {
-			isFocused: false,
-		};
-	},
-	componentWillMount () {
+		this.getCodeMirror = this.getCodeMirror.bind(this);
+		this.getCodeMirrorInstance = this.getCodeMirrorInstance.bind(this);
+		this.focus = this.focus.bind(this);
+		this.focusChanged = this.focusChanged.bind(this);
+		this.scrollChanged = this.scrollChanged.bind(this);
+		this.codemirrorValueChanged = this.codemirrorValueChanged.bind(this);
+	}
+
+
+	componentWillMount() {
 		this.componentWillReceiveProps = debounce(this.componentWillReceiveProps, 0);
-	},
-	componentDidMount () {
-		const textareaNode = findDOMNode(this.refs.textarea);
-		const codeMirrorInstance = this.getCodeMirrorInstance();
+	}
+
+	componentDidMount() {
+		var textareaNode = findDOMNode(this.refs.textarea);
+		var codeMirrorInstance = this.getCodeMirrorInstance();
 		this.codeMirror = codeMirrorInstance.fromTextArea(textareaNode, this.props.options);
 		this.codeMirror.on('change', this.codemirrorValueChanged);
 		this.codeMirror.on('focus', this.focusChanged.bind(this, true));
 		this.codeMirror.on('blur', this.focusChanged.bind(this, false));
 		this.codeMirror.on('scroll', this.scrollChanged);
 		this.codeMirror.setValue(this.props.defaultValue || this.props.value || '');
-	},
-	componentWillUnmount () {
+	}
+
+	componentWillUnmount() {
 		// is there a lighter-weight way to remove the cm instance?
 		if (this.codeMirror) {
 			this.codeMirror.toTextArea();
 		}
-	},
-	componentWillReceiveProps: function (nextProps) {
+	}
+
+	componentWillReceiveProps(nextProps) {
 		if (this.codeMirror && nextProps.value !== undefined && normalizeLineEndings(this.codeMirror.getValue()) !== normalizeLineEndings(nextProps.value)) {
 			if (this.props.preserveScrollPosition) {
 				var prevScrollPosition = this.codeMirror.getScrollInfo();
@@ -65,47 +60,75 @@ const CodeMirror = React.createClass({
 			}
 		}
 		if (typeof nextProps.options === 'object') {
-			for (let optionName in nextProps.options) {
+			for (var optionName in nextProps.options) {
 				if (nextProps.options.hasOwnProperty(optionName)) {
 					this.codeMirror.setOption(optionName, nextProps.options[optionName]);
 				}
 			}
 		}
-	},
-	getCodeMirror () {
+	}
+
+	getCodeMirrorInstance() {
+		return this.props.codeMirrorInstance || require('codemirror');
+	}
+
+	getCodeMirror() {
 		return this.codeMirror;
-	},
-	focus () {
+	}
+
+	focus() {
 		if (this.codeMirror) {
 			this.codeMirror.focus();
 		}
-	},
-	focusChanged (focused) {
+	}
+
+	focusChanged(focused) {
 		this.setState({
-			isFocused: focused,
+			isFocused: focused
 		});
 		this.props.onFocusChange && this.props.onFocusChange(focused);
-	},
-	scrollChanged (cm) {
+	}
+
+	scrollChanged(cm) {
 		this.props.onScroll && this.props.onScroll(cm.getScrollInfo());
-	},
-	codemirrorValueChanged (doc, change) {
+	}
+
+	codemirrorValueChanged(doc, change) {
 		if (this.props.onChange && change.origin !== 'setValue') {
 			this.props.onChange(doc.getValue(), change);
 		}
-	},
-	render () {
-		const editorClassName = className(
-			'ReactCodeMirror',
-			this.state.isFocused ? 'ReactCodeMirror--focused' : null,
-			this.props.className
-		);
-		return (
-			<div className={editorClassName}>
-				<textarea ref="textarea" name={this.props.path} defaultValue={this.props.value} autoComplete="off" />
-			</div>
-		);
-	},
-});
+	}
 
-module.exports = CodeMirror;
+	render() {
+		var editorClassName = className('ReactCodeMirror', this.state.isFocused ? 'ReactCodeMirror--focused' : null, this.props.className);
+		return React.createElement(
+			'div',
+			{className: editorClassName},
+			React.createElement('textarea', {
+				ref: 'textarea',
+				name: this.props.path,
+				defaultValue: this.props.value,
+				autoComplete: 'off'
+			})
+		);
+	}
+}
+
+Codemirror.propTypes = {
+	className: PropTypes.any,
+	codeMirrorInstance: PropTypes.func,
+	defaultValue: PropTypes.string,
+	onChange: PropTypes.func,
+	onFocusChange: PropTypes.func,
+	onScroll: PropTypes.func,
+	options: PropTypes.object,
+	path: PropTypes.string,
+	value: PropTypes.string,
+	preserveScrollPosition: PropTypes.bool
+};
+Codemirror.defaultProps = {
+	preserveScrollPosition: false
+};
+Codemirror.displayName = 'CodeMirror';
+
+export default Codemirror;
